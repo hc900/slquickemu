d5b895fc0044ea5134c129e53ef783d86fcee4f3
HC <hc@emmybeararcades.com>
2020-11-01 18:35:13 -0500

    add copyrights / gpl to config and utils

--- src/qemuconfig.rs
+++ src/qemuconfig.rs
@@ -1,3 +1,28 @@
+/*
+ *
+        Copyright (C) 2020 Emmybear Arcades
+
+        This program is free software; you can redistribute it and/or
+        modify it under the terms of the GNU General Public License
+        as published by the Free Software Foundation; version 2
+        of the License.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+        See the GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program; if not, write to:
+
+                Free Software Foundation, Inc.
+                59 Temple Place - Suite 330
+                Boston, MA  02111-1307, USA
+
+ *
+ */
+
 use glob::glob;
 use serde::{Deserialize,Serialize};
 use std::path::Path;
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,29 @@
+/*
+ *
+        Copyright (C) 2020 Emmybear Arcades
+
+        This program is free software; you can redistribute it and/or
+        modify it under the terms of the GNU General Public License
+        as published by the Free Software Foundation; version 2
+        of the License.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+        See the GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program; if not, write to:
+
+                Free Software Foundation, Inc.
+                59 Temple Place - Suite 330
+                Boston, MA  02111-1307, USA
+
+ *
+ */
+
+
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 use std::net::TcpStream;
